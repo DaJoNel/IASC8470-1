846011055def51234a0356c5029b7fc3ff733878
David Nelson <dnelson@unomaha.edu>
2016-08-30 10:43:09 -0700

    End of task 15; using Ember store.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 var Photo = Ember.Object.extend({
 	title: '',
 	username: '',
-	url: '',
+	url: function(){
+		return "https://farm"+this.get('farm')+
+		".staticflickr.com/"+this.get('server')+
+		"/"+this.get('id')+"_"+this.get('secret')+"_b.jpg";
+	}.property('farm','server','id','secret'),
 	//flickr extra data
 	owner: '',
 	//flickr url data
@@ -11,11 +15,6 @@ var Photo = Ember.Object.extend({
 	farm: 0,
 	secret: '',
 	server: '',
-	url: function(){
-		return "https://farm"+this.get('farm')+
-		".staticflickr.com/"+this.get('server')+
-		"/"+this.get('id')+"_"+this.get('secret')+"_b.jpg";
-	}.property('farm','server','id','secret'),
 });
 
 var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
@@ -24,39 +23,10 @@ var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
 	content: [],
 });
 
-var testPhotos = PhotoCollection.create();
-var testimg1 = Photo.create({
-	title: "Google logo",
-	username: "google",
-	url: "https://www.google.com/images/srpr/logo11w.png"
-});
-
-var testimg2 = Photo.create({
-	title: "UNO logo",
-	username: "UNO",
-	url: "http://www.unomaha.edu/_files/images/logo-subsite-o-2.png"
-});
-
-var testimg3 = Photo.create({
-	title: "Facebook Logo",
-	username: "Facebook",
-	url: "https://www.facebook.com/images/fb_icon_325x325.png"
-});
-
-var testimg4 = Photo.create({
-	title: "Hubble Carina Nebula",
-	username: "NASA",
-	url: "http://imgsrc.hubblesite.org/hu/db/images/hs-2010-13-a-1920x1200_wallpaper.jpg"
-});
-
-testPhotos.pushObject(testimg1);
-testPhotos.pushObject(testimg2);
-testPhotos.pushObject(testimg3);
-testPhotos.pushObject(testimg4);
-
 export default Ember.Controller.extend({
-	photos: testPhotos,
+	photos: PhotoCollection.create(),
 	searchField: '',
+	tagSearchField: '',
 	filteredPhotos: function () {
 		var filter = this.get('searchField');
 		var rx = new RegExp(filter, 'gi');
@@ -67,18 +37,23 @@ export default Ember.Controller.extend({
 		});
 	}.property('photos.@each','searchField'),
 	actions: {
-		getPhotos: function(){
+		search: function () {
+			this.get('photos').content.clear();
+			this.store.unloadAll('photo');
+			this.send('getPhotos',this.get('tagSearchField'));
+		},
+		getPhotos: function(tag){
 			var apiKey = '60e5e96ee3938f6098c823357dc12ede';
 			var host = 'https://api.flickr.com/services/rest/';
 			var method = "flickr.tags.getClusterPhotos";
-			var tag = "hi";
 			var requestURL = host + "?method="+method + "&api_key="+apiKey+"&tag="+tag+"&format=json&nojsoncallback=1";
 			var photos = this.get('photos');
+			var t = this;
 			Ember.$.getJSON(requestURL, function(data){
 				//callback for successfully completed requests
 				console.log(data);
 				data.photos.photo.map(function(photo) {
-					var newPhotoItem = Photo.create({
+					var newPhotoItem = t.store.createRecord('photo',{
 						title: photo.title,
 						username: photo.username,
 						//flickr extra data
@@ -90,11 +65,8 @@ export default Ember.Controller.extend({
 						server: photo.server,
 					});
 					photos.pushObject(newPhotoItem);
-				})
+				});
 			});
-		},
-		search: function () {
-			this.get('filteredPhotos');
 		}
 	}
 });
